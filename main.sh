360ed8b456a93e70e49fe03864652e9cf7656d26
Martin Wheeler <martin@martinwheeler.com.au>
2024-10-13 13:25:18 +1000

    feat: also update server list

--- main.sh
+++ main.sh
@@ -1,7 +1,11 @@
 cwd=$(pwd)
+
+# NOTE: This may be a different path depending on where the user has installed it
 steam="C:\Program Files (x86)\Steam\steam.exe"
 
-base_path="$HOME/Zomboid/Saves/Multiplayer"
+base_path="$HOME/Zomboid"
+save_path="$base_path/Saves/Multiplayer"
+lua_path="$base_path/Lua"
 
 # Check for old ip address file
 if [ -e old_ip_address.txt ]; then
@@ -12,7 +16,6 @@ else
 fi
 
 old_ip_address=$(cat "$cwd"/old_ip_address.txt)
-# TODO: Fetch this from an API
 
 if [ -z "$1" ]; then
   new_ip_address=$(curl https://raw.githubusercontent.com/martinwheeler/project-zomboid/refs/heads/main/current_ip.txt)
@@ -23,20 +26,24 @@ fi
 echo "Old IP: $old_ip_address"
 echo "New IP: $new_ip_address"
 
-old_folders=$(ls $base_path | grep $old_ip_address)
+old_folders=$(ls $save_path | grep $old_ip_address)
 
 # There may be multiple characters so move them all
 for old_folder in $old_folders; do
   new_folder=$(echo $old_folder | sed -e "s/$old_ip_address/$new_ip_address/")
-  mv -f "${base_path}/${old_folder}" "${base_path}/${new_folder}"
+  mv -f "${save_path}/${old_folder}" "${save_path}/${new_folder}"
 done
 
 echo "Finished migrating player data"
 
-echo $new_ip_address >old_ip_address.txt
+echo "Updating PZ server list with new IP"
+
+sed -i -e "s/$old_ip_address/$new_ip_address/" "$lua_path/ServerListSteam.txt"
 
 echo "Saving IP address for next time"
 
-echo "Launching PZ"
+echo $new_ip_address >old_ip_address.txt
+
+echo "Launching PZ!"
 
 "$steam" steam://rungameid/108600
